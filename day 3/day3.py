75557346b5f0195a4d3865d2a470f2502325b96b
Jonatan A. Martini <martini.j.andre@gmail.com>
2023-01-06 22:36:07 -0300

    guideline

--- day 3/day3.py
+++ day 3/day3.py
@@ -23,17 +23,30 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-direction = str(input("You're in a forest and you see a cave, and you feel like you should enter it. After walking for a few minutes you can see it goes in two different directions, type 'left' if you want to go left or type 'right' if you want to go right."))
+direction = str(input("You're in a forest and you see a cave, and you feel like you should enter it. After walking "
+                      "for a few minutes you can see it goes in two different directions, type 'left' if you want to "
+                      "go left or type 'right' if you want to go right."))
 
 if direction == "left":
-  print("The cave goes deeper and you can smell something that's not that good and you start to regret your choice, but it's already too late as you see two fierce eyes get near your face and you realize it's your end. Game over.")
+    print("The cave goes deeper and you can smell something that's not that good and you start to regret your choice, "
+          "but it's already too late as you see two fierce eyes get near your face and you realize it's your end. Game "
+          "over.")
 elif direction == "right":
-  yell = str(input("The cave goes foreward and you end up in a place that seems like a very large room, you can yell something to anlyze the echo or to see if something shows up (type 'yes') or you can go further and try to recognize the place by hand (type 'no')."))
-  if yell == "yes":
-    print("You can hear some steps and you start to smell something that's not that good and you regret your choice, but it's already too late as you turn around and see two fierce eyes get near your face and you realize it's your end. Game over.")
-  elif yell == "no":
-    run_or_walk = str(input("You go to the end of the cave to see a chest opened up, revealing its treasure. You take the treasure and turn around in order to get out of the cave. You can do it silently like nothing happened or you can run to guarantee your treasure quickly. Type 'run' or type 'walk'."))
+    yell = str(input("The cave goes forward and you end up in a place that seems like a very large room, you can yell "
+                     "something to analyze the echo or to see if something shows up (type 'yes') or you can go further "
+                     "and try to recognize the place by hand (type 'no')."))
+    if yell == "yes":
+        print("You can hear some steps and you start to smell something that's not that good and you regret your "
+              "choice, but it's already too late as you turn around and see two fierce eyes get near your face and you "
+              "realize it's your end. Game over.")
+    elif yell == "no":
+        run_or_walk = str(input("You go to the end of the cave to see a chest opened up, revealing its treasure. You "
+                                "take the treasure and turn around in order to get out of the cave. You can do it "
+                                "silently like nothing happened or you can run to guarantee your treasure quickly. "
+                                "Type 'run' or type 'walk'."))
     if run_or_walk == "run":
-      print("As you're running you can hear some loud steps getting closer and you regret your choice, but it's already too late as you turn around and see two fierce eyes get near your face and you realize it's your end. Game over.")
+        print("As you're running you can hear some loud steps getting closer and you regret your choice, but it's "
+              "already too late as you turn around and see two fierce eyes get near your face and you realize it's "
+              "your end. Game over.")
     elif run_or_walk == "walk":
-      print("You get out of the cave safely with your treasure. Congratulations, you won the game!")
+        print("You get out of the cave safely with your treasure. Congratulations, you won the game!")
