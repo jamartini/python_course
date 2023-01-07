b3fbab7d633d2d2ba2f3a018c19db31ea4d61b95
Jonatan A. Martini <martini.j.andre@gmail.com>
2023-01-06 22:41:32 -0300

    guideline

--- day 4/day4.py
+++ day 4/day4.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,43 +27,41 @@ scissors = '''
 ---.__(___)
 '''
 
-import random
-
 your_choice = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors.\n"))
 if your_choice == 0:
-  print(rock)
+    print(rock)
 elif your_choice == 1:
-  print(paper)
+    print(paper)
 elif your_choice == 2:
-  print(scissors)
+    print(scissors)
 
 print("Computer chose:")
-computer_choice = random.randint(0,2)
+computer_choice = random.randint(0, 2)
 if computer_choice == 0:
-  print(rock)
+    print(rock)
 elif computer_choice == 1:
-  print(paper)
+    print(paper)
 elif computer_choice == 2:
-  print(scissors)
+    print(scissors)
 
 if your_choice == 0:
-  if computer_choice == 0:
-    print("That's a draw.")
-  elif computer_choice == 1:
-    print("You lost.")
-  elif computer_choice == 2:
-    print("You won.")
+    if computer_choice == 0:
+        print("That's a draw.")
+    elif computer_choice == 1:
+        print("You lost.")
+    elif computer_choice == 2:
+        print("You won.")
 elif your_choice == 1:
-  if computer_choice == 0:
-    print("You won.")
-  elif computer_choice == 1:
-    print("That's a draw.")
-  elif computer_choice == 2:
-    print("You lost.")
+    if computer_choice == 0:
+        print("You won.")
+    elif computer_choice == 1:
+        print("That's a draw.")
+    elif computer_choice == 2:
+        print("You lost.")
 elif your_choice == 2:
-  if computer_choice == 0:
-    print("You lost.")
-  elif computer_choice == 1:
-    print("You won.")
-  elif computer_choice == 2:
-    print("That's a draw.")
+    if computer_choice == 0:
+        print("You lost.")
+    elif computer_choice == 1:
+        print("You won.")
+    elif computer_choice == 2:
+        print("That's a draw.")
